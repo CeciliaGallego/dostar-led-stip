942e80282c329973a5ca44477fae04aec683da62
CeciliaGallego <cecilia.gallegocarracedo@gmail.com>
2025-08-21 13:56:22 +0100

    Fix bugs and add bilateral functionality

--- code.py
+++ code.py
@@ -1,34 +1,61 @@
-# Test with the light
 import board
 import busio
 import adafruit_dotstar as dotstar
 
+N_DOTS = 72
+BILATERAL = True # LEDs will turn on in both sides
 
-dots = dotstar.DotStar(board.GP10, board.GP11, 144, brightness=0.1)
+dots = dotstar.DotStar(board.GP14, board.GP15, N_DOTS, brightness=0.1)
 
 uart = busio.UART(board.GP0, board.GP1, baudrate=9600)
 
 # # # INITIALIZATION
-dots.fill((255, 0, 0))
-n_dots = len(dots)
+dots.fill((0, 0, 0))
 
 # # # MAIN LOOP
 while True:
 
-    data = uart.read(1)
-
-    if data and len(data) == 1:
+    data = uart.read(1)    
+    uart_value = data[0]
         
-        index = data[0]
+    # Turn on the corresponding LED
+    if 0 <= uart_value <= 100:
         
-        if 1 <= index < n_dots-1:
-            dots[index-1] = (255, 255, 255) 
-            dots[index] = (255, 255, 255) 
-            dots[index+1] = (255, 255, 255) 
-        # # Edge cases
-        elif index == 0:
-            dots[index] = (255, 255, 255) 
-            dots[index+1] = (255, 255, 255) 
-        elif index == n_dots-1:
-            dots[index-1] = (255, 255, 255) 
-            dots[index] = (255, 255, 255)
+        if BILATERAL:
+            # Convert the UART value to an index for the dots
+            index = int((uart_value / 100) * (N_DOTS/2 - 2))+1
+            
+            # # Set the dot at the calculated index to white
+            if 0 < index < N_DOTS-1:
+                dots.fill((255, 0, 0))
+                dots[index-1] = (255, 255, 255) 
+                dots[index] = (255, 255, 255) 
+                dots[-index-1] = (255, 255, 255) 
+                dots[-index] = (255, 255, 255) 
+            
+        else:
+            # Convert the UART value to an index for the dots
+            index = int((uart_value / 100) * (N_DOTS - 3))+1
+            
+            # # Set the dot at the calculated index to white
+            if 0 < index < N_DOTS-1:
+                dots.fill((255, 0, 0))
+                dots[index-1] = (255, 255, 255) 
+                dots[index] = (255, 255, 255) 
+                dots[index+1] = (255, 255, 255)
+    
+    # Turn off all dots
+    if uart_value == 200:
+        dots.fill((0, 0, 0))
+        
+    # Turn on all dots to red
+    if uart_value == 201:
+        dots.fill((255, 0, 0)) 
+       
+    # Turn on bilateral mode
+    if uart_value == 210:
+        BILATERAL = True
+            
+    # Turn off bilateral mode
+    if uart_value == 211:
+        BILATERAL = False
