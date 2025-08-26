bf0d90a4bd7d1b375adbf93267c847f3ea545af6
CeciliaGallego <cecilia.gallegocarracedo@gmail.com>
2025-08-26 17:15:26 +0100

    0

--- test_code.py
+++ test_code.py
@@ -5,7 +5,7 @@ from time import sleep
 
 N_DOTS = 72
 
-dots = dotstar.DotStar(board.GP10, board.GP11, N_DOTS, brightness=0.1)
+dots = dotstar.DotStar(board.GP2, board.GP3, N_DOTS, brightness=0.1)
 
 # # # INITIALIZATION
 dots.fill((255, 0, 0))
@@ -15,6 +15,6 @@ while True:
 
     for i in range(N_DOTS):
         dots.fill((255, 0, 0))
-        dots[i] = (0, 0, 0)
+        dots[i] = (255, 255, 255)
         
         sleep(0.5)
