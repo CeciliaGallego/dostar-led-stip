224e6c3e248352c392f4945077575febe87e3ffd
CeciliaGallego <cecilia.gallegocarracedo@gmail.com>
2026-01-12 17:02:41 +0000

    small improvements

--- code_v2.py
+++ code_v2.py
@@ -14,7 +14,7 @@ dots = dotstar.DotStar(board.GP2, board.GP3, N_DOTS, brightness=0.1)
 uart = busio.UART(board.GP0, board.GP1, baudrate=9600)
 
 # # # INITIALIZATION
-dots.fill((0, 0, 0))
+# dots.fill((0, 0, 0))
 on_idx = 1
 
 # # # MAIN LOOP
@@ -35,25 +35,25 @@ while True:
             continue
         
         # Turn on all dots to red and start leds
-        if uart_value == 201:
+        elif uart_value == 201:
             dots.fill((255, 0, 0)) 
             LEDS_OFF = False
         
         # Turn off all dots and stop leds
-        if uart_value == 200:
+        elif uart_value == 200:
             dots.fill((0, 0, 0))
             LEDS_OFF = True
             
         # Turn on bilateral mode
-        if uart_value == 210:
+        elif uart_value == 210:
             BILATERAL = True
                 
         # Turn off bilateral mode
-        if uart_value == 211:
+        elif uart_value == 211:
             BILATERAL = False
             
         # Turn on the corresponding LED
-        if 0 < uart_value <= 100:
+        elif 0 < uart_value <= 100:
             
             if BILATERAL:
                 # Convert the UART value to an index for the dots
